3752a6da71a5022b15c94c703a8c2fcce0ca072b
Marco Stoffel <marco.stoffel@softwareag.com>
2024-05-02 17:10:45 +0200

    Added config node

--- src/nodes/c8y-config/c8y-config.js
+++ src/nodes/c8y-config/c8y-config.js
@@ -1,15 +1,15 @@
-const c8yClientLib = require('@c8y/client');
-const createDeviceandAddExternalId = require("../c8y-utils/c8y-utils");
-const util = require("util");
 
 module.exports = function(RED) {
     function GetInternalIdCallNode(config) {
         RED.nodes.createNode(this,config);
         this.c8ytenant = config.c8ytenant;
         this.c8yurl = config.c8yurl;
-        this.c8ytenant = config.c8ytenant;
-        this.c8ytenant = config.c8ytenant;
     }
-    RED.nodes.registerType("c8y-config", GetInternalIdCallNode);
+    RED.nodes.registerType("c8yconfig", GetInternalIdCallNode, {
+      credentials: {
+        c8yuser: { type: "text" },
+        c8ypassword: { type: "password" },
+      },
+    });
 }
 
--- src/nodes/get-internal-id/get-internal-id.js
+++ src/nodes/get-internal-id/get-internal-id.js
@@ -7,38 +7,65 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.config = config;
-        const {C8Y_TENANT,C8Y_BASEURL, C8Y_USER,C8Y_PASSWORD} = process.env;
-        const auth = new c8yClientLib.BasicAuth({tenant: C8Y_TENANT, user: C8Y_USER, password: C8Y_PASSWORD});
-
+        
         node.on('input', async function(msg) {
-          node.client = new c8yClientLib.Client(auth, C8Y_BASEURL);
-          node.client.core.tenant = C8Y_TENANT;
-          
           try {
+            node.c8yconfig = RED.nodes.getNode(node.config.c8yconfig);
+            if (node.c8yconfig) {
+              node.C8Y_TENANT = node.c8yconfig.c8ytenant;
+            }else{
+              node.error('No config found');
+              return;
+            }
+            if (node.C8Y_TENANT == 'env'){
+              node.C8Y_TENANT = process.env.C8Y_TENANT;
+              node.C8Y_BASEURL = process.env.C8Y_BASEURL;
+              node.C8Y_USER = process.env.C8Y_USER;
+              node.C8Y_PASSWORD = process.env.C8Y_PASSWORD;
+            }else{
+              node.C8Y_TENANT = node.c8yconfig.c8ytenant;
+              node.C8Y_BASEURL = node.c8yconfig.c8yurl;
+              node.C8Y_USER = node.c8yconfig.credentials.c8yuser;
+              node.C8Y_PASSWORD = node.c8yconfig.credentials.c8ypassword;
+            }
+            const tenant = node.C8Y_TENANT;
+            const baseUrl = node.C8Y_BASEURL;
+            const user = node.C8Y_USER;
+            const password = node.C8Y_PASSWORD;
+            const auth = new c8yClientLib.BasicAuth({
+              tenant,
+              user,
+              password,
+            });
+            node.client = new c8yClientLib.Client(auth, node.C8Y_BASEURL);
+            node.client.core.tenant = node.C8Y_TENANT;
             // Get properties
             externalIdType = RED.util.evaluateNodeProperty(
               node.config.externalidtype,
               node.config.externalidtypeType,
               node,
               msg
-            );
-            externalId = RED.util.evaluateNodeProperty(
-              node.config.externalid,
-              node.config.externalidType,
-              node,
-              msg
-            );
-            params = RED.util.evaluateNodeProperty(
-              node.config.params,
-              node.config.paramsType,
-              node,
-              msg
-            );
-          } catch (error) {
-            node.error("Extracting Properties " +error);
-            return;
-          }
-
+              );
+              externalId = RED.util.evaluateNodeProperty(
+                node.config.externalid,
+                node.config.externalidType,
+                node,
+                msg
+                );
+              if (node.config.createdevice) {
+                params = RED.util.evaluateNodeProperty(
+                  node.config.params,
+                  node.config.paramsType,
+                  node,
+                  msg
+                  );
+              }
+                  node.log("Config: " + node.C8Y_TENANT + node.C8Y_BASEURL + node.C8Y_PASSWORD + node.C8Y_USER);
+                } catch (error) {
+                  node.error("Extracting Properties " +error);
+                  return;
+                }
+                
           node.debug("Config: externalId: " + externalId +  " externalIdType: " + externalIdType )
           
           if (externalId === undefined) {
